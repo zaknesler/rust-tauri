d140eada24e2f905c9f71a968bcc86abf33f7607
Zak Nesler <zak@nesler.dev>
2023-03-16 19:54:31 -0400

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,20 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn paths(path: &str) -> Vec<String> {
-    let files = std::fs::read_dir(path)
-        .unwrap()
-        .into_iter()
-        .map(|v| {
-            let file_name = v.unwrap().file_name();
-            let val = file_name.as_os_str().to_str().unwrap_or("");
-            val.to_string()
-        })
-        .collect::<Vec<_>>();
-
-    files
-}
+mod error;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![paths])
+        .invoke_handler(tauri::generate_handler![list_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn list_dir(path: &str) -> Result<Vec<String>, error::Error> {
+    let files = std::fs::read_dir(path)?;
+
+    Ok(files
+        .filter_map(|file| file.ok())
+        .map(|file| file.file_name().into_string().unwrap())
+        .collect::<Vec<_>>())
 }
